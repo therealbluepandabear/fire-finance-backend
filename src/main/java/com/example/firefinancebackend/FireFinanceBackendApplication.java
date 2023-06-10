e0b0ff096c62ca71daad07fa11a6f78ebebc5b1d
therealbluepandabear <therealbluepandabear@protonmail.com>
2023-06-10 16:20:03 +1200

    Initial commit

---
+++ src/main/java/com/example/firefinancebackend/FireFinanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.firefinancebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireFinanceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireFinanceBackendApplication.class, args);
+    }
+
+}
